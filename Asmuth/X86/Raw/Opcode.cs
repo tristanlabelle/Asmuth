d228e3d2e1ad6e8991a07c42f0c9985cdaf05714
Tristan Labelle <tristan@tristanlabelle.com>
2016-01-27 22:33:29 -0800

    Added Opcode.LookupKey_Mask

--- Asmuth/X86/Raw/Opcode.cs
+++ Asmuth/X86/Raw/Opcode.cs
@@ -72,6 +72,11 @@ namespace Asmuth.X86.Raw
 
 		_3DNow_Shift = ExtraByte_Shift,
 		_3DNow_Mask = ExtraByte_Mask,
+
+		// When doing lookups, we can only consider parts of the opcode
+		// that unambiguously identify an operation.
+		// We have to ignore: simd prefixes, primary nibble, xex flags, extra bytes
+		LookupKey_Mask = XexType_Mask | Map_Mask | MainByte_High4Mask,
 	}
 
 	public enum SimdPrefix : byte
