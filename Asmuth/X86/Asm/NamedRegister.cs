6b80c279e32de4267e5caa92ff8315d0a6044a87
Tristan Labelle <tristan@tristanlabelle.com>
2018-08-02 09:29:50 +0200

    Created Asmuth.X86.Asm namespace. Renamed OperandSpec to OperandFormat and added more types.

--- Asmuth/X86/Asm/NamedRegister.cs
+++ Asmuth/X86/Asm/NamedRegister.cs
@@ -4,7 +4,7 @@ using System.Diagnostics.Contracts;
 using System.Runtime.InteropServices;
 using System.Text;
 
-namespace Asmuth.X86
+namespace Asmuth.X86.Asm
 {
 	public enum RegisterNamespace : byte
 	{
--- Asmuth/X86/Asm/OperandFormat.cs
+++ Asmuth/X86/Asm/OperandFormat.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Text;
 
-namespace Asmuth.X86
+namespace Asmuth.X86.Asm
 {
 	public abstract class OperandFormat
 	{
@@ -12,6 +12,7 @@ namespace Asmuth.X86
 		public abstract override string ToString();
 	}
 
+	// PUSH CS
 	public sealed class FixedRegOperand : OperandFormat
 	{
 		public NamedRegister Register { get; }
@@ -21,6 +22,7 @@ namespace Asmuth.X86
 		public override string ToString() => Register.ToString();
 	}
 
+	// PUSH r32
 	public sealed class RegOperand : OperandFormat
 	{
 		// Gpr8_High: AL, CL, DL, BL, AH, CH, DH, BH (Legacy with no REX prefix)
@@ -33,13 +35,14 @@ namespace Asmuth.X86
 
 		public override string ToString() => throw new NotImplementedException();
 
-		public static readonly RegOperand Gpr8_LowOnly = new RegOperand(RegisterNamespace.Gpr8_Low);
-		public static readonly RegOperand Gpr8_LowHigh = new RegOperand(RegisterNamespace.Gpr8_High);
+		public static readonly RegOperand Gpr8_NoRex = new RegOperand(RegisterNamespace.Gpr8_High);
+		public static readonly RegOperand Gpr8_Rex = new RegOperand(RegisterNamespace.Gpr8_Low);
 		public static readonly RegOperand Gpr16 = new RegOperand(RegisterNamespace.Gpr16);
 		public static readonly RegOperand Gpr32 = new RegOperand(RegisterNamespace.Gpr32);
 		public static readonly RegOperand Gpr64 = new RegOperand(RegisterNamespace.Gpr64);
 	}
 
+	// FDIV m32fp
 	public sealed class MemOperand : OperandFormat
 	{
 		// TODO: Data type
@@ -58,6 +61,7 @@ namespace Asmuth.X86
 		public static readonly MemOperand M128 = new MemOperand(16);
 	}
 
+	// NEG r/m8
 	public sealed class RegOrMemOperand : OperandFormat
 	{
 		public RegOperand RegSpec { get; }
@@ -75,19 +79,21 @@ namespace Asmuth.X86
 
 		public override string ToString() => throw new NotImplementedException();
 
-		public static readonly RegOrMemOperand RM8_LowOnly = new RegOrMemOperand(RegOperand.Gpr8_LowOnly, MemOperand.M8);
-		public static readonly RegOrMemOperand RM8_LowHigh = new RegOrMemOperand(RegOperand.Gpr8_LowHigh, MemOperand.M8);
+		public static readonly RegOrMemOperand RM8_NoRex = new RegOrMemOperand(RegOperand.Gpr8_NoRex, MemOperand.M8);
+		public static readonly RegOrMemOperand RM8_Rex = new RegOrMemOperand(RegOperand.Gpr8_Rex, MemOperand.M8);
 		public static readonly RegOrMemOperand RM16 = new RegOrMemOperand(RegOperand.Gpr16, MemOperand.M16);
 		public static readonly RegOrMemOperand RM32 = new RegOrMemOperand(RegOperand.Gpr32, MemOperand.M32);
 		public static readonly RegOrMemOperand RM64 = new RegOrMemOperand(RegOperand.Gpr64, MemOperand.M64);
 	}
 
+	// PUSH imm32 
 	public sealed class ImmOperand : OperandFormat
 	{
 		// TODO: Data type
 		public override string ToString() => "imm";
 	}
-	
+
+	// SAL r/m8, 1 
 	public sealed class ConstOperand : OperandFormat
 	{
 		public sbyte Value { get; }
@@ -98,5 +104,19 @@ namespace Asmuth.X86
 
 		public static readonly ConstOperand Zero = new ConstOperand(0);
 		public static readonly ConstOperand One = new ConstOperand(1);
+	}
+	
+	// JMP rel8
+	public sealed class RelOperand : OperandFormat
+	{
+		public OperandSize OffsetSize { get; }
+
+		public RelOperand(OperandSize offsetSize) => this.OffsetSize = offsetSize;
+
+		public override string ToString() => "rel" + OffsetSize.InBits();
+
+		public static readonly RelOperand Rel8 = new RelOperand(OperandSize.Byte);
+		public static readonly RelOperand Rel16 = new RelOperand(OperandSize.Word);
+		public static readonly RelOperand Rel32 = new RelOperand(OperandSize.Qword);
 	}
 }
